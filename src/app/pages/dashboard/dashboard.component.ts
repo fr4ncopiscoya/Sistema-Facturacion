03894851eadfbbc483b9ef56d8594aafc68c7372
Franco Piscoya <francomazns@gmail.com>
2024-05-27 12:12:35 -0500

    feat(dashboard): Se agregaron tables a dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppComponent } from '../../app.component';
+import { Config } from 'datatables.net';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
   constructor(
     private appComponent: AppComponent,
   ) {
     this.appComponent.login = false;
+  }
+
+  dtOptions: Config = {};
+  dtOptionsVendidos: Config = {};
+
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      scrollY: '162px'
+    };
+    this.dtOptionsVendidos = {
+      pagingType: 'full_numbers',
+      scrollY: '162px'
+    };
   }
 
 }
